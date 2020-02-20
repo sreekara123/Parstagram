5964f463479ae42199037f9d73d2f3109641913a
Sreekara Yachamaneni <syachama@purdue.edu>
2020-02-20 15:14:54 -0500

    Initial Commit

---
+++ Parstagram/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Parstagram
+//
+//  Created by Sreekara Yachamaneni on 2/20/20.
+//  Copyright © 2020 Sreekara Yachamaneni. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
